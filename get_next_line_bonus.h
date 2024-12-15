5721941deb0adcda1fef60c118430928c3115dcb
Cyl <cyl.sites@orange.fr>
2024-12-15 17:35:03 +0100

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8
+# endif
+
+# define FD_MAX 1024
+
+typedef struct s_varstr
+{
+	char	*str;
+	size_t	len;
+}	t_varstr;
+
+typedef struct s_readbuffer
+{
+	char	*linestart;
+	char	buffer[BUFFER_SIZE];
+}	t_readbuffer;
+
+void	varstr_init(t_varstr *varstr);
+char	*varstr_free(t_varstr *varstr);
+char	*varstr_add(t_varstr *varstr, char *str2_start, char *str2_end);
+char	*get_next_line(int fd);
+
+#endif
